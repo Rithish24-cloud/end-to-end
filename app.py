d87f419d578f2010510c351f5e43d792e8e7cbd3
Rithish24-cloud <rithishkumar2405@gmail.com>
2026-04-14 22:13:24 +0530

    hiii

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask DevOps Project! it is safe?"
+    return "Hello from Flask DevOps Project! it is safe?  i have this "
 
 @app.route("/about")
 def about():
